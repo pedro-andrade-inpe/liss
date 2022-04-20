75221a7ef9600a37c2fb3d63eca871cf3932b79b
Pedro R. Andrade <pedro.andrade@inpe.br>
2022-04-19 23:15:05 -0300

    updating columns with problems

--- add-icon-column.R
+++ add-icon-column.R
@@ -3,8 +3,29 @@
 basePath <- "C:/Users/pedro/Dropbox/github/liss"
 
 campos <-list.dirs(basePath, recursive = FALSE)
-mfile <- paste0(campos[5], "/points.shp")
-msf <- sf::read_sf(mfile)
+
+# santarem
+mfile <- paste0(basePath, "/santarem/points.shp")
+msf <- sf::read_sf(mfile) %>%
+  dplyr::mutate(Foto_CMM = ifelse(is.na(Foto_CMM), "NO", Foto_CMM)) %>%
+  dplyr::mutate(Foto_Uso = ifelse(is.na(Foto_Uso), "NO", Foto_Uso)) %>%
+  dplyr::mutate(id = paste(id))
 
 msf$micon = "home"
+msf$micon2 = 1
+sf::write_sf(msf, mfile)
+
+
+# tocantins
+mfile <- paste0(basePath, "/baixoTocantins/points.shp")
+
+msf <- sf::read_sf(mfile) %>%
+  dplyr::mutate(drone_path = ifelse(is.na(drone_path), "NO", drone_path)) %>%
+  dplyr::mutate(Uso = ifelse(is.na(Uso), "NO", Uso)) %>%
+  dplyr::mutate(pic_path = ifelse(is.na(pic_path), "NO", pic_path))
+
+msf
+
+msf$micon = "home"
+msf$micon2 = 1
 sf::write_sf(msf, mfile)
