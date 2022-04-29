fca0dc2d60191e12fefa99fe7b59a41b1745e10c
Pedro R. Andrade <pedro.andrade@inpe.br>
2022-04-28 22:12:50 -0300

    Adding dates and photos to the main app

--- application.lua
+++ application.lua
@@ -2,6 +2,25 @@
 
 import("publish")
 
+descriptions = {
+    arapiuns = {
+        date = "Realizado no período de 4 a 15 de junho de 2012.",
+        photo = {"Arapiuns.jpg"},
+    },
+    baixoTocantins = {
+        date = "Realizado nos anos de 2018 e 2019.",
+        photo = {"Baixo Tocantins 2018.jpg", "Baixo Tocantins 2019.jpg"},
+    },
+    BR230 = {
+        date = "Realizado no período de 6 a 26 de setembro de 2013.",
+        photo = {"BR230.jpg"}
+    },
+    santarem = {
+        date = "Realizado no período de 6 a 26 de setembro de 2013.",
+        photo = {"Santarem.jpg"}
+    }
+}
+
 local description = [[
 	This webpage describes LiSS fieldworks in the Brazilian Amazonia. Each of these fieldworks was funded by a set of agencies, shown in the respective application. Please click in a box to see more information and to navigate through specific applications.<br>The toolkit to create this application was funded by FAPESP...
 ]]
@@ -27,7 +46,13 @@ Application{
             local link = "https://combinatronics.com/pedro-andrade-inpe/liss/main/"..
               cell.app.."/"..cell.app.."WebMap/index.html"
 
+            report:addText(descriptions[cell.app].date)
+
 			report:addText("See the application <a href = \""..link.."\" target=\"_blank\">here</a>.")
+
+            forEachElement(descriptions[cell.app].photo, function(_, value)
+              report:addImage("photos/"..value)
+            end)
 
 			return report
 		end
